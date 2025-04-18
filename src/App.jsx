798a54fe59189ee8ab4dc2328ad50527942a4b1f
Maleesha Sandakalum <maleeshaGitHub0000@gmail.com>
2025-04-18 17:24:04 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import styles from './App.module.css'
 import { Chat } from './components/Chat/Chat'
 import { Controls } from './components/Controls/Controls'
-import { Assistant } from './assistants/googleai'
+import { Assistant as Gemini } from './assistants/googleai'
+import { Assistant as OpenAi } from './assistants/openai'
 
 function App() {
-  const assistant = new Assistant()
+  const selectedAi = useRef();
+  const geminiAssistant = new Gemini()
+  const openAiAssistant = new OpenAi()
   const WELCOME_MESSAGE = {
     role: 'assistant',
     content: 'Hello! How can I assist you right now?',
@@ -17,10 +20,21 @@ function App() {
     setMessages((prevMessages) => [...prevMessages, message])
   }
 
-  async function handleContentSend(content) {
+  async function handleGeminiContentSend(content) {
     addMessage({ content: content, role: 'user' })
     try {
-      const result = await assistant.chat(content)
+      const result = await geminiAssistant.chat(content)
+      addMessage({ content: result, role: 'assistant' });
+    } catch (error) {
+      console.error(error)
+      addMessage({ content: "Sorry I couldn't process your request. please try again.", role: 'system' });
+    }
+  }
+
+  async function handleOpenAiContentSend(content) {
+    addMessage({ content: content, role: 'user' })
+    try {
+      const result = await openAiAssistant.chat(content, messages)
       addMessage({ content: result, role: 'assistant' });
     } catch (error) {
       console.error(error)
@@ -32,12 +46,25 @@ function App() {
     <div className={styles.App}>
       <header className={styles.Header}>
         <img className={styles.Logo} src="/chat-bot.png" />
-        <h2 className={styles.Title}>AI Chatbot</h2>
+        <h2 className={styles.Title}>AI Chatbot
+        </h2>
+        <select className={styles.AiSelection} ref={selectedAi}>
+          <option value="Gemini">Gemini</option>
+          <option value="OpenAI">OpenAI</option>
+          <option value="D-seek">Deep-seek</option>
+        </select>
       </header>
       <div className={styles.ChatContainer}>
         <Chat messages={messages} />
       </div>
-      <Controls onSend={handleContentSend} />
+      <Controls onSend={(content) => {
+        const ai = selectedAi.current.value
+        if (ai === 'Gemini') {
+          handleGeminiContentSend(content)
+        } else if (ai === 'OpenAI') {
+          handleOpenAiContentSend(content)
+        }
+      }} />
     </div>
   )
 }
