0d4f1b8a36bf1502cecf07097c876e9a1a1b0701
Maleesha Sandakalum <maleeshaGitHub0000@gmail.com>
2025-04-18 22:19:37 +0530

    changes

--- src/assistants/openai.js
+++ src/assistants/openai.js
@@ -22,7 +22,26 @@ export class Assistant {
             })
 
             return result.choices[0].message.content;
-            
+
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
+
+    async *chatStream(content, history) {
+        try {
+            const result = await openai.chat.completions.create({
+                model: this.#model,
+                messages: [...history, { content, role: 'user' }],
+                stream: true,
+            })
+
+            for await (const chunk of result) {
+                yield chunk.choices[0]?.delta?.content || "";
+            }
+
+            return result.choices[0].message.content;
+
         } catch (error) {
             throw new Error(error);
         }
